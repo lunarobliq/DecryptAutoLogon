68fcaa0d17afaadfec836313d0ec43f378477c06
ceramicskate0 <64501203+ceramic-skate0@users.noreply.github.com>
2021-07-13 12:35:12 -0400

    Update AssemblyInfo.cs

--- DecryptAutoLogon/Properties/AssemblyInfo.cs
+++ DecryptAutoLogon/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DecryptAutoLogon")]
+[assembly: AssemblyTitle("DecryptAutoLog")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DecryptAutoLogon")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("DecryptAutoLog")]
+[assembly: AssemblyCopyright("Copyright ©  2000")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
